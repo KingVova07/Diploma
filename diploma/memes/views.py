1e757e732df2ed74867ebcdd8bec60a208a23bb9
TheKoligor <vovahunter2000@gmail.com>
2021-04-11 14:58:03 +0300

    Add django

--- diploma/memes/views.py
+++ diploma/memes/views.py
@@ -13,7 +13,7 @@ def index(request,page = 1):
     Memes = []
     pages = []
     for i in range(page-2,page+3):
-        if i <= 1 or i > 144:
+        if i <= 1 or i > 104:
             continue
         pages.append(i)
 
@@ -28,4 +28,27 @@ def index(request,page = 1):
                   })
 
 def memes(request,meme_id):
-    return HttpResponse("Hello world")
+    meme_name = meme_id
+    image =  New_MEMES[meme_id]["image"]
+    views = New_MEMES[meme_id]["views"]
+    origin_year =  New_MEMES[meme_id]["origin_year"]
+    date_added =  New_MEMES[meme_id]["date_added"]
+    videos =  New_MEMES[meme_id]["videos"]
+    photos =  New_MEMES[meme_id]["photos"]
+    comments =  New_MEMES[meme_id]["comments"]
+    about =  New_MEMES[meme_id]["about"]
+    origin =  New_MEMES[meme_id]["origin"]
+    other_text =  New_MEMES[meme_id]["other_text"]
+    try:
+        origin_year = int(origin_year)
+    except:
+        origin_year = "Unknown"
+    return render(request, "memes/memes.html",
+                      {
+                        "name": meme_name, "image" : image,
+                        "origin_year": origin_year,
+                        "date_added": date_added, "views": views,
+                        "videos": videos, "photos": photos, "comments": comments,
+                        "about": about, "origin": origin,
+                        "other_text": other_text
+                      })
